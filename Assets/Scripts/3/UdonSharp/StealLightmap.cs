69e6cf1519971a39c946cb91f275d2f4d2c96800
Float3 <86748455+Float3@users.noreply.github.com>
2021-09-10 10:18:25 +0200

    fixed ShaderPreProcessor.cs added StealLightmap.cs

---
+++ Assets/Scripts/3/UdonSharp/StealLightmap.cs
@@ -0,0 +1,42 @@
+using UdonSharp;
+using UnityEngine;
+
+namespace _3.UdonSharp
+{
+    [ExecuteInEditMode]
+    public class StealLightmap : UdonSharpBehaviour
+    {
+        public MeshRenderer lightmappedObject;
+        private MeshRenderer currentRenderer;
+
+        private void Awake()
+        {
+            currentRenderer = gameObject.GetComponent<MeshRenderer>();
+            RendererInfoTransfer();
+        }
+
+        private void OnEnable()
+        {
+            Awake();
+        }
+
+#if UNITY_EDITOR
+        private void OnBecameVisible()
+        {
+            RendererInfoTransfer();
+        }
+#endif
+
+        private void RendererInfoTransfer()
+        {
+            if (lightmappedObject == null || currentRenderer == null)
+                return;
+
+            currentRenderer.lightmapIndex = lightmappedObject.lightmapIndex;
+            currentRenderer.lightmapScaleOffset = lightmappedObject.lightmapScaleOffset;
+            currentRenderer.realtimeLightmapIndex = lightmappedObject.realtimeLightmapIndex;
+            currentRenderer.realtimeLightmapScaleOffset = lightmappedObject.realtimeLightmapScaleOffset;
+            currentRenderer.lightProbeUsage = lightmappedObject.lightProbeUsage;
+        }
+    }
+}
