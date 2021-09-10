e380848828e37332013755424f26db883e46e6c9
Float3 <86748455+Float3@users.noreply.github.com>
2021-09-10 10:49:06 +0200

    fixed StealLightmap.cs added StealLightmap.asset

--- Assets/Scripts/3/UdonSharp/StealLightmap.cs
+++ Assets/Scripts/3/UdonSharp/StealLightmap.cs
@@ -1,3 +1,4 @@
+#if UDON
 using UdonSharp;
 using UnityEngine;
 
@@ -7,6 +8,49 @@ namespace _3.UdonSharp
     public class StealLightmap : UdonSharpBehaviour
     {
         public MeshRenderer lightmappedObject;
+        private MeshRenderer currentRenderer;
+
+        private void Start()
+        {
+            currentRenderer = gameObject.GetComponent<MeshRenderer>();
+            RendererInfoTransfer();
+        }
+
+        private void OnEnable()
+        {
+            Start();
+        }
+
+#if UNITY_EDITOR
+        private void OnBecameVisible()
+        {
+            RendererInfoTransfer();
+        }
+#endif
+
+        private void RendererInfoTransfer()
+        {
+            if (lightmappedObject == null || currentRenderer == null)
+                return;
+
+            currentRenderer.lightmapIndex = lightmappedObject.lightmapIndex;
+            currentRenderer.lightmapScaleOffset = lightmappedObject.lightmapScaleOffset;
+            currentRenderer.realtimeLightmapIndex = lightmappedObject.realtimeLightmapIndex;
+            currentRenderer.realtimeLightmapScaleOffset = lightmappedObject.realtimeLightmapScaleOffset;
+            currentRenderer.lightProbeUsage = lightmappedObject.lightProbeUsage;
+        }
+    }
+}
+#else
+using UnityEngine;
+
+namespace _3.Mono
+{
+    [ExecuteInEditMode]
+    public class StealLightmap : MonoBehaviour
+    {
+        public MeshRenderer lightmappedObject;
+
         private MeshRenderer currentRenderer;
 
         private void Awake()
@@ -40,3 +84,4 @@ namespace _3.UdonSharp
         }
     }
 }
+#endif
