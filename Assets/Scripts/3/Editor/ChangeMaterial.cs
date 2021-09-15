0c0657303d75bc91e6cc4c7e8c4563f1d426a9cc
Float3 <86748455+Float3@users.noreply.github.com>
2021-09-15 15:55:56 +0200

    gitDumb

--- Assets/Scripts/3/Editor/ChangeMaterial.cs
+++ Assets/Scripts/3/Editor/ChangeMaterial.cs
@@ -6,88 +6,88 @@ using UnityEngine;
 
 namespace _3.Editor
 {
-    public sealed class MaterialMigrator : EditorWindow
-    {
-        private Material selectedMaterial;
-        private SerializedObject serializedObject;
-        private Shader yourShader;
+	public sealed class MaterialMigrator : EditorWindow
+	{
+		private Material selectedMaterial;
+		private SerializedObject serializedObject;
+		private Shader yourShader;
 
-        private void OnEnable()
-        {
-            GetSelectedMaterial();
-        }
+		private void OnEnable()
+		{
+			GetSelectedMaterial();
+		}
 
-        private void OnGUI()
-        {
-            EditorGUIUtility.labelWidth = 200f;
+		private void OnGUI()
+		{
+			EditorGUIUtility.labelWidth = 200f;
 
-            yourShader = (Shader) EditorGUILayout.ObjectField("Shader", yourShader, typeof(Shader), false);
-
-
-            if (selectedMaterial == null)
-            {
-                EditorGUILayout.LabelField("No material selected");
-            }
-            else
-            {
-                serializedObject.Update();
+			yourShader = (Shader) EditorGUILayout.ObjectField("Shader", yourShader, typeof(Shader), false);
 
 
-                EditorGUILayout.Space();
-                EditorGUILayout.LabelField("Selected material:", selectedMaterial.name);
-                if (GUILayout.Button("Migrate mats"))
-                    MigrateMultiple(yourShader);
-            }
+			if (selectedMaterial == null)
+			{
+				EditorGUILayout.LabelField("No material selected");
+			}
+			else
+			{
+				serializedObject.Update();
 
 
-            EditorGUIUtility.labelWidth = 0;
-        }
-
-        private void OnProjectChange()
-        {
-            GetSelectedMaterial();
-        }
-
-        private void OnSelectionChange()
-        {
-            GetSelectedMaterial();
-        }
+				EditorGUILayout.Space();
+				EditorGUILayout.LabelField("Selected material:", selectedMaterial.name);
+				if (GUILayout.Button("Migrate mats"))
+					MigrateMultiple(yourShader);
+			}
 
 
-        [MenuItem("Window/Material Migrator")]
-        private static void Init()
-        {
-            GetWindow<MaterialMigrator>("Mat. Migrator");
-        }
+			EditorGUIUtility.labelWidth = 0;
+		}
 
-        [MenuItem("Tools/3/Migrate Materials to lit")]
-        private void GetSelectedMaterial()
-        {
-            selectedMaterial = Selection.activeObject as Material;
-            if (selectedMaterial != null) serializedObject = new SerializedObject(selectedMaterial);
+		private void OnProjectChange()
+		{
+			GetSelectedMaterial();
+		}
 
-            Repaint();
-        }
+		private void OnSelectionChange()
+		{
+			GetSelectedMaterial();
+		}
 
-        private static void MigrateMultiple(Shader p_shader)
-        {
-            foreach (var obj in Selection.objects)
-            {
-                var mat = obj as Material;
-                if (mat != null)
-                {
-                    MigrateMaterial(mat, p_shader);
-                    Debug.Log("debug");
-                }
-            }
-        }
 
-        private static void MigrateMaterial(Material p_material, Shader p_shader)
-        {
-            var storedQueue = p_material.renderQueue;
-            p_material.shader = p_shader;
-            p_material.renderQueue = storedQueue;
-        }
-    }
+		[MenuItem("Window/Material Migrator")]
+		private static void Init()
+		{
+			GetWindow<MaterialMigrator>("Mat. Migrator");
+		}
+
+		[MenuItem("Tools/3/Migrate Materials to lit")]
+		private void GetSelectedMaterial()
+		{
+			selectedMaterial = Selection.activeObject as Material;
+			if (selectedMaterial != null) serializedObject = new SerializedObject(selectedMaterial);
+
+			Repaint();
+		}
+
+		private static void MigrateMultiple(Shader p_shader)
+		{
+			foreach (var obj in Selection.objects)
+			{
+				var mat = obj as Material;
+				if (mat != null)
+				{
+					MigrateMaterial(mat, p_shader);
+					//Debug.Log("debug");
+				}
+			}
+		}
+
+		private static void MigrateMaterial(Material p_material, Shader p_shader)
+		{
+			var storedQueue = p_material.renderQueue;
+			p_material.shader = p_shader;
+			p_material.renderQueue = storedQueue;
+		}
+	}
 }
 #endif
