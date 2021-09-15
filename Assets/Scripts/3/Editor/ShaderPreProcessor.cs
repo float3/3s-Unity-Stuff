419922bdc4b668d07f0e915256054300ec0a0aea
3 <3>
2021-09-15 10:52:24 +0200

    Revert "start converting https://bottosson.github.io/posts/colorpicker/#common-code"
    This reverts commit 7220ee16c5cf3867a8caf6b581a22d48a20c1dbf.

--- Assets/Scripts/3/Editor/ShaderPreProcessor.cs
+++ Assets/Scripts/3/Editor/ShaderPreProcessor.cs
@@ -10,32 +10,32 @@ using UnityEngine.Rendering;
 
 namespace _3.Editor
 {
-	internal class ShaderPreProcessor : IPreprocessShaders
-	{
-		private readonly PassType[] pts =
-		{
-			PassType.Deferred, PassType.LightPrePassBase, PassType.LightPrePassFinal, PassType.VertexLM, PassType.Meta
-		};
+    internal class ShaderPreProcessor : IPreprocessShaders
+    {
+        private readonly PassType[] pts =
+        {
+            PassType.Deferred, PassType.LightPrePassBase, PassType.LightPrePassFinal, PassType.VertexLM, PassType.Meta
+        };
 
-		public int callbackOrder => 0;
+        public int callbackOrder => 1;
 
-		public void OnProcessShader(Shader p_shader, ShaderSnippetData p_snippet, IList<ShaderCompilerData> p_data)
-		{
-			if (pts.Contains(p_snippet.passType))
-			{
-				//Debug.Log($"Consumed {p_shader.name} = {p_snippet.passType} = {p_snippet.passName}");
-				p_data.Clear();
-				return;
-			}
+        public void OnProcessShader(Shader p_shader, ShaderSnippetData p_snippet, IList<ShaderCompilerData> p_data)
+        {
+            if (pts.Contains(p_snippet.passType))
+            {
+                //Debug.Log($"Consumed {p_shader.name} = {p_snippet.passType} = {p_snippet.passName}");
+                p_data.Clear();
+                return;
+            }
 
-			var shaderName = p_shader.name;
-			shaderName = string.IsNullOrEmpty(shaderName) ? "Empty" : shaderName;
-			if (shaderName.Contains("Hidden/PostProcessing"))
-			{
-				//Debug.Log($"Consumed {p_shader.name}");
-				p_data.Clear();
-			}
-		}
-	}
+            var shaderName = p_shader.name;
+            shaderName = string.IsNullOrEmpty(shaderName) ? "Empty" : shaderName;
+            if (shaderName.Contains("Hidden/PostProcessing"))
+            {
+                //Debug.Log($"Consumed {p_shader.name}");
+                p_data.Clear();
+            }
+        }
+    }
 }
 #endif
