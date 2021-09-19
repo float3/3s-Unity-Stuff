4e5e089a2338e3eda33562e7f66db9a9b1ffaaac
3 <86748455+Float3@users.noreply.github.com>
2021-09-19 21:30:27 +0200

    fixed

--- Assets/Scripts/3/Editor/PostProcessingPreProcessor.cs
+++ Assets/Scripts/3/Editor/PostProcessingPreProcessor.cs
@@ -13,7 +13,6 @@ namespace _3.Editor
 		public int callbackOrder { get { return 0; } }
 		public void OnPreprocessBuild(BuildReport report)
 		{
-			path += @"/Library/PackageCache/com.unity.postprocessing@3.1.1/PostProcessing";
 			Directory.Delete(Path.GetFullPath("Packges/com.unity.postprocessing@3.1.1/PostProcessing/Shaders"));
 			Directory.Delete(Path.GetFullPath("Packages/com.unity.postprocessing@3.1.1/PostProcessing/Textures"));
 			Directory.Delete(Path.GetFullPath("Library/PackageCache/com.unity.postprocessing@3.1.1/PostProcessing/Shaders"));
