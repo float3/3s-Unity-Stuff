da9c787741a6e3f6e1cbf419ffd974b757313188
3 <86748455+Float3@users.noreply.github.com>
2021-09-20 09:15:13 +0200

    try catch fix?

--- Assets/Scripts/3/Editor/PostProcessingPreProcessor.cs
+++ Assets/Scripts/3/Editor/PostProcessingPreProcessor.cs
@@ -8,18 +8,54 @@ using UnityEngine;
 
 namespace _3.Editor
 {
-	internal class AssetPreProcessor : IPreprocessBuildWithReport
+	internal class PostProcessingPreProcessor : IPreprocessBuildWithReport
 	{
 		public int callbackOrder { get { return 0; } }
 		public void OnPreprocessBuild(BuildReport report)
 		{
-			try{
-				Directory.Delete(Path.GetFullPath("Packges/com.unity.postprocessing@3.1.1/PostProcessing/Shaders"));
+			Debug.Log("try Packages/");
+			try
+			{
+				Directory.Delete(Path.GetFullPath("Packages/com.unity.postprocessing@3.1.1/PostProcessing/Shaders"));
 				Directory.Delete(Path.GetFullPath("Packages/com.unity.postprocessing@3.1.1/PostProcessing/Textures"));
+			}
+			catch(IOException e) 
+			{
+				Debug.Log("IOException");
+				Debug.Log(e);
+			}
+			catch(UnauthorizedAccessException e)
+			{
+				Debug.Log("UnauthorizedAccessException");
+				Debug.Log(e);
+			}
+			catch(DirectoryNotFoundException e)
+			{
+				Debug.Log("DirectoryNotFoundException");
+				Debug.Log(e);
+			}
+
+			Debug.Log("try Library/PackageCache");
+			try
+			{
 				Directory.Delete(Path.GetFullPath("Library/PackageCache/com.unity.postprocessing@3.1.1/PostProcessing/Shaders"));
 				Directory.Delete(Path.GetFullPath("Library/PackageCache/com.unity.postprocessing@3.1.1/PostProcessing/Textures"));
 			}
-			catch(){}
+			catch(IOException e) 
+			{
+				Debug.Log("IOException");
+				Debug.Log(e);
+			}
+			catch(UnauthorizedAccessException e)
+			{
+				Debug.Log("UnauthorizedAccessException");
+				Debug.Log(e);
+			}
+			catch(DirectoryNotFoundException e)
+			{
+				Debug.Log("DirectoryNotFoundException");
+				Debug.Log(e);
+			}
 	}	
 }
 #endif
