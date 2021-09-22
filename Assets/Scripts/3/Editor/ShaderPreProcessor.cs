9d30d4d6d52a89a202aca30784d18e6268f08870
3 <86748455+Float3@users.noreply.github.com>
2021-09-22 15:37:04 +0200

    IVRCSDKPreprocessAvatarCallback + IVRCSDKBuildRequestedCallback test

--- Assets/Scripts/3/Editor/ShaderPreProcessor.cs
+++ Assets/Scripts/3/Editor/ShaderPreProcessor.cs
@@ -1,4 +1,4 @@
-﻿#if UNITY_EDITOR
+﻿//#if UNITY_EDITOR
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -9,23 +9,60 @@ using UnityEditor.Rendering;
 
 namespace _3.Editor
 {
+
+	public class OnAvatarBuild : IVRCSDKPreprocessAvatarCallback
+	{
+		public int callbackOrder => 3;
+
+		public bool OnPreprocessAvatars(Gameobject avatarGameobject)
+		{
+			Debug.Log("OnPreProcessAvatars" + avatarGameobject);
+			return true;
+		}
+	}
+
+	public class OnSceneBuild : IVRCSDKBuildRequestedCallback
+	{
+		public int callbackOrder => 6;
+
+		public bool OnBuildRequested(VRCSDKRequestedBuildType requestedBuildType)
+		{
+			Debug.Log("OnBuildRequested" + requestedBuildType)
+			if (requestedBuildType == VRCSDKRequestedBuildType.Avatar)
+			{
+#define AVATAR
+				return true;
+			}
+			else
+			{
+#undef AVATAR
+				return true;
+			}
+		}
+	}
+
+
+
 	public class PreprocessShaders : IPreprocessShaders
 	{
-	#if (VRC_SDK_VRCSDK3 && UDON) || VRC_SDK_SDK2
-			public PassType[] pts = {PassType.Deferred, PassType.LightPrePassBase, PassType.LightPrePassFinal, PassType.VertexLM, PassType.MotionVectors, PassType.ScriptableRenderPipeline, PassType.ScriptableRenderPipelineDefaultUnlit};
-	#elif VRC_SDK_VRCSDK3 && !UDON
-			public PassType[] pts = {PassType.Deferred, PassType.LightPrePassBase, PassType.LightPrePassFinal, PassType.VertexLM, PassType.Meta, PassType.MotionVectors, PassType.ScriptableRenderPipeline, PassType.ScriptableRenderPipelineDefaultUnlit};
-	#endif
 
-		
-		public int callbackOrder => 3;
+		public PassType[] pts = {PassType.Deferred, PassType.LightPrePassBase, PassType.LightPrePassFinal, PassType.VertexLM, PassType.MotionVectors, PassType.ScriptableRenderPipeline, PassType.ScriptableRenderPipelineDefaultUnlit};
+
+		public int callbackOrder => 9;
 
 		public void OnProcessShader(Shader shader, ShaderSnippetData snippet, IList<ShaderCompilerData> data)
 		{
+			Debug.Log("OnProcessShader")
+#if AVATAR
+		Debug.Log("AVATAR");
+		public PassType[] ptsfinal = {pts + PassType.Normal};
+#else
+		Debug.Log("ELSE");
+		public PassType[] ptsfinal = pts;
+#endif
+#undef AVATAR
 
-			Debug.Log("OnProcessShader"); //remember to test which callback happens first
-
-			if (pts.Contains(snippet.passType))
+			if (ptsfinal.Contains(snippet.passType) )
 			{
 				//Debug.Log($"Consumed {shader.name} = {snippet.passType} = {snippet.passName}");
 				data.Clear();
@@ -35,21 +72,13 @@ namespace _3.Editor
 			string shaderName = shader.name;
 			shaderName = string.IsNullOrEmpty(shaderName) ? "Empty" : shaderName;
 			if (shaderName.Contains("Hidden/PostProcessing"))
+			{
 				//Debug.Log($"Consumed {shader.name}");
 				data.Clear();
+			}
 		}
-	}
 
-	public  class class VRCCallbackTest : IVRCSDKBuildRequestedCallback
-	{
-		public int callbackOrder => 3;
-
-		bool IVRCSDKBuildRequestedCallback.OnBuildRequested(VRCSDKRequestedBuildType requestedBuildType)
-		{
-			Debug.Log($"OnBuildRequest {requestedBuildType}")
-			return true;
-		}
 	}
 }
 
-#endif
+//#endif
