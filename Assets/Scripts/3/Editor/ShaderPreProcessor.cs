fbebf3b267f3cfdf7edff418ac2ef82431fd5749
3 <86748455+Float3@users.noreply.github.com>
2021-09-22 10:10:53 +0200

    update to ShaderPreProcessor, also TODO Debug.Log test which callback happens first

--- Assets/Scripts/3/Editor/ShaderPreProcessor.cs
+++ Assets/Scripts/3/Editor/ShaderPreProcessor.cs
@@ -1,32 +1,30 @@
 ﻿#if UNITY_EDITOR
-
-#region
-
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine;
 using UnityEditor.Build;
 using UnityEditor.Rendering;
-using UnityEngine;
-using UnityEngine.Rendering;
-
-#endregion
 
 // thank you Scruffy and z3y
 
 namespace _3.Editor
 {
-	internal class AutoLockOnBuild : IPreprocessShaders
+	public class PreprocessShaders : IPreprocessShaders
 	{
-		public PassType[] pts =
-		{
-			PassType.Deferred, PassType.LightPrePassBase, PassType.LightPrePassFinal, PassType.VertexLM, PassType.Meta,
-			PassType.MotionVectors, PassType.ScriptableRenderPipeline, PassType.ScriptableRenderPipelineDefaultUnlit
-		};
+	#if (VRC_SDK_VRCSDK3 && UDON) || VRC_SDK_SDK2
+			public PassType[] pts = {PassType.Deferred, PassType.LightPrePassBase, PassType.LightPrePassFinal, PassType.VertexLM, PassType.MotionVectors, PassType.ScriptableRenderPipeline, PassType.ScriptableRenderPipelineDefaultUnlit};
+	#elif VRC_SDK_VRCSDK3 && !UDON
+			public PassType[] pts = {PassType.Deferred, PassType.LightPrePassBase, PassType.LightPrePassFinal, PassType.VertexLM, PassType.Meta, PassType.MotionVectors, PassType.ScriptableRenderPipeline, PassType.ScriptableRenderPipelineDefaultUnlit};
+	#endif
 
+		
 		public int callbackOrder => 3;
 
 		public void OnProcessShader(Shader shader, ShaderSnippetData snippet, IList<ShaderCompilerData> data)
 		{
+
+			Debug.Log("OnProcessShader"); //remember to test which callback happens first
+
 			if (pts.Contains(snippet.passType))
 			{
 				//Debug.Log($"Consumed {shader.name} = {snippet.passType} = {snippet.passName}");
@@ -34,11 +32,22 @@ namespace _3.Editor
 				return;
 			}
 
-			var shaderName = shader.name;
+			string shaderName = shader.name;
 			shaderName = string.IsNullOrEmpty(shaderName) ? "Empty" : shaderName;
 			if (shaderName.Contains("Hidden/PostProcessing"))
 				//Debug.Log($"Consumed {shader.name}");
 				data.Clear();
+		}
+	}
+
+	public  class class VRCCallbackTest : IVRCSDKBuildRequestedCallback
+	{
+		public int callbackOrder => 3;
+
+		bool IVRCSDKBuildRequestedCallback.OnBuildRequested(VRCSDKRequestedBuildType requestedBuildType)
+		{
+			Debug.Log($"OnBuildRequest {requestedBuildType}")
+			return true;
 		}
 	}
 }
