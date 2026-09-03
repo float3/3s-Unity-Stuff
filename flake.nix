45c472f0476c14a869b3e54add25446066ee3bd8
hill <hill@hilll.dev>
2026-09-03 09:07:44 +0200

    Add Nix flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "3s-Unity-Stuff";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      formatter = pkgs.alejandra;
+
+      devShells.default = pkgs.mkShell {
+        packages = [
+          pkgs.dotnet-sdk_8
+        ];
+      };
+    });
+}
